63b9c4231026f11c70bd8e21939515a96a2368a0
Laura <llizara1@my.centennialcollege.a>
2023-07-14 19:47:53 -0400

    First draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { SalesComponent } from './components/sales/sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'chat', pathMatch: 'full' },
+  {path: 'login', component:LoginComponent },
+  {path: 'dashboard', component:DashboardComponent },
+  {path: 'employees', component:EmployeesComponent },
+  {path: 'sales', component: SalesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { SalesComponent } from './components/sales/sales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    EmployeesComponent,
+    SalesComponent
   ],
   imports: [
     BrowserModule,
